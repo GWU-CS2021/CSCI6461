f25c5cb96ce9d258aebec7e7f4dd768f98e254c7
Sugarman-Z <18301781817@163.com>
2022-03-13 13:30:57 -0400

    debug and pass all test

--- project/src/assemble.py
+++ project/src/assemble.py
@@ -1,19 +1,24 @@
 import logging
 from op_code_list import OpCodeList
 
+
 class Assemble:
 
     def assemble(self, file_path="instruction.txt"):
-        # print(format(int(OpCodeList.JGE, 16), "b"))
         logging.info("Assemble start working.")
         binary_instruction_list = []
+        hex_instruction_list = []
         with open(file_path) as instruction_reader:
             for line in instruction_reader:
                 line = line.strip("\n")
                 line = self.convert_instruction_to_binary(line)
+                if len(line) > 16:
+                    raise TypeError("Invalid operation code.")
                 binary_instruction_list.append(line)
+                line = format(int(line, 2), "04x")
+                hex_instruction_list.append(line)
         logging.info("Assemble ends.")
-        return binary_instruction_list
+        return binary_instruction_list, hex_instruction_list
 
     def convert_instruction_to_binary(self, data_list):
         data_list = self.preprocess(data_list)
@@ -26,7 +31,7 @@ class Assemble:
             binary_string += "1100000000000000"
         # format as r,x,address,i
         if instruction in OpCodeList.Group1:
-            binary_string += format(int(OpCodeList.Group1[instruction], 16), "06b")
+            binary_string += format(int(OpCodeList.Group1[instruction], 8), "06b")
             binary_string += format(int(data_list[1]), "02b")
             binary_string += format(int(data_list[2]), "02b")
             if len(data_list) >= 5:
@@ -36,7 +41,7 @@ class Assemble:
             binary_string += format(int(data_list[3]), "05b")
         # format as x,address,i
         elif instruction in OpCodeList.Group2:
-            binary_string += format(int(OpCodeList.Group2[instruction], 16), "06b")
+            binary_string += format(int(OpCodeList.Group2[instruction], 8), "06b")
             binary_string += "00"
             binary_string += format(int(data_list[1]), "02b")
             if len(data_list) >= 4:
@@ -46,24 +51,27 @@ class Assemble:
             binary_string += format(int(data_list[2]), "05b")
         # format as immediate
         elif instruction in OpCodeList.Group3:
-            binary_string += format(int(OpCodeList.Group3[instruction], 16), "06b")
+            binary_string += format(int(OpCodeList.Group3[instruction], 8), "06b")
             binary_string += "00000"
             binary_string += format(int(data_list[1]), "05b")
         # format as r, immediate
         elif instruction in OpCodeList.Group4:
-            binary_string += format(int(OpCodeList.Group4[instruction], 16), "06b")
+            binary_string += format(int(OpCodeList.Group4[instruction], 8), "06b")
             binary_string += format(int(data_list[1]), "02b")
             binary_string += "000"
             binary_string += format(int(data_list[2]), "05b")
         # format as rx, ry
         elif instruction in OpCodeList.Group5:
-            binary_string += format(int(OpCodeList.Group5[instruction], 16), "06b")
+            binary_string += format(int(OpCodeList.Group5[instruction], 8), "06b")
             binary_string += format(int(data_list[1]), "02b")
-            binary_string += format(int(data_list[2]), "02b")
+            if len(data_list) == 3:
+                binary_string += format(int(data_list[2]), "02b")
+            else:
+                binary_string += "00"
             binary_string += "000000"
         # format as r, count , L/R, A/L
         elif instruction in OpCodeList.Group6:
-            binary_string += format(int(OpCodeList.Group6[instruction], 16), "06b")
+            binary_string += format(int(OpCodeList.Group6[instruction], 8), "06b")
             binary_string += format(int(data_list[1]), "02b")
             binary_string += format(int(data_list[4]), "01b")
             binary_string += format(int(data_list[3]), "01b")
@@ -71,13 +79,13 @@ class Assemble:
             binary_string += format(int(data_list[2]), "05b")
         # format as r, deviceId
         elif instruction in OpCodeList.Group7:
-            binary_string += format(int(OpCodeList.Group7[instruction], 16), "06b")
+            binary_string += format(int(OpCodeList.Group7[instruction], 8), "06b")
             binary_string += format(int(data_list[1]), "02b")
             binary_string += "000"
             binary_string += format(int(data_list[2]), "05b")
         # format as fr, x, address, i
         elif instruction in OpCodeList.Group8:
-            binary_string += format(int(OpCodeList.Group8[instruction], 16), "06b")
+            binary_string += format(int(OpCodeList.Group8[instruction], 8), "06b")
             binary_string += format(int(data_list[1]), "02b")
             binary_string += format(int(data_list[2]), "02b")
             if len(data_list) >= 5:
@@ -99,9 +107,22 @@ class Assemble:
         data_list[0] = data_list[0].upper()
         return data_list
 
+
 def main():
     test = Assemble()
-    print(test.assemble())
+    binary_instruction_list = []
+    all_pass = True
+    with open("Test_Case_Binary.txt") as binary_instruction:
+        for line in binary_instruction:
+            line = line.strip("\n")
+            binary_instruction_list.append(line)
+    assemble_binary_list, assemble_hex_list = test.assemble()
+    for i in range(len(assemble_binary_list)):
+        if assemble_binary_list[i] != binary_instruction_list[i]:
+            print(i, assemble_binary_list[i], binary_instruction_list[i])
+            all_pass = False
+    print(all_pass)
+
 
 if __name__ == "__main__":
     main()
