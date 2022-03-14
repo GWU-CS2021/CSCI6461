823eab035cb08b8b60623e1a897aea54f2b33605
reimu <bilibili10086@gmail.com>
2022-03-13 20:49:41 -0400

    refactor delivery

--- delivery-phase1/delivery/delivery/run_gui.bat
+++
@@ -1,5 +0,0 @@
-cd "%~dp0"
-simulator_GUI\simulator_GUI.exe
-cls
-echo [101;93m if something is wrong with this script, please use simulator_GUI\simulator_GUI.exe to start the program mannually. [0m
-timeout /t 30
--- delivery-phase1/delivery/delivery/source_code/project/src/cache.py
+++
@@ -1,5 +0,0 @@
-# cache hit -> read, renew counter
-# cache miss -> read from memory and write cache
-# cache update(upon memory update) -> renew
-# cache write(upon store to memory) -> write to cache
-# cache full -> kick LRU line
